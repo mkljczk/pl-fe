5a3ede16130c703be9ffed2fd2c01cc48af46346
marcin mikołajczak <git@mkljczk.pl>
2024-09-06 09:09:58 +0200

    pl-fe: improve logo line spacing
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- packages/pl-fe/src/features/landing-timeline/components/site-banner.tsx
+++ packages/pl-fe/src/features/landing-timeline/components/site-banner.tsx
@@ -13,16 +13,18 @@ const SiteBanner: React.FC = () => {
   const description = DOMPurify.sanitize(instance.description);
 
   return (
-    <Stack space={3}>
+    <Stack space={6}>
       <LogoText dir={getTextDirection(instance.title)}>
         {instance.title}
       </LogoText>
 
-      <Markup
-        size='lg'
-        dangerouslySetInnerHTML={{ __html: description }}
-        direction={getTextDirection(description)}
-      />
+      {instance.description.trim().length > 0 && (
+        <Markup
+          size='lg'
+          dangerouslySetInnerHTML={{ __html: description }}
+          direction={getTextDirection(description)}
+        />
+      )}
     </Stack>
   );
 };
