72efcada651aa20432e201cbd95d769bbab5bbe9
marcin mikołajczak <git@mkljczk.pl>
2024-10-20 13:22:50 +0200

    pl-fe: Remove unused action
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- packages/pl-fe/src/reducers/meta.test.ts
+++ packages/pl-fe/src/reducers/meta.test.ts
@@ -1,7 +1,5 @@
 import { Record as ImmutableRecord } from 'immutable';
 
-import { SW_UPDATING, setSwUpdating } from 'pl-fe/actions/sw';
-
 import reducer from './meta';
 
 describe('meta reducer', () => {
@@ -10,12 +8,5 @@ describe('meta reducer', () => {
     expect(ImmutableRecord.isRecord(result)).toBe(true);
     expect(result.instance_fetch_failed).toBe(false);
     expect(result.swUpdating).toBe(false);
-  });
-
-  describe(SW_UPDATING, () => {
-    it('sets swUpdating to the provided value', () => {
-      const result = reducer(undefined, setSwUpdating(true));
-      expect(result.swUpdating).toBe(true);
-    });
   });
 });
--- packages/pl-fe/src/reducers/meta.ts
+++ packages/pl-fe/src/reducers/meta.ts
@@ -1,24 +1,19 @@
 import { Record as ImmutableRecord } from 'immutable';
 
 import { INSTANCE_FETCH_FAIL, type InstanceAction } from 'pl-fe/actions/instance';
-import { SW_UPDATING, type SwAction } from 'pl-fe/actions/sw';
 
 const ReducerRecord = ImmutableRecord({
   /** Whether /api/v1/instance 404'd (and we should display the external auth form). */
   instance_fetch_failed: false,
-  /** Whether the ServiceWorker is currently updating (and we should display a loading screen). */
-  swUpdating: false,
 });
 
-const meta = (state = ReducerRecord(), action: InstanceAction | SwAction) => {
+const meta = (state = ReducerRecord(), action: InstanceAction) => {
   switch (action.type) {
     case INSTANCE_FETCH_FAIL:
       if (action.error?.response?.status === 404) {
         return state.set('instance_fetch_failed', true);
       }
       return state;
-    case SW_UPDATING:
-      return state.set('swUpdating', action.isUpdating);
     default:
       return state;
   }
