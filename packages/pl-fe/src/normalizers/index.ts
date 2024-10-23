60a7865420bdced15c175a8b91c524890ac987ff
marcin mikołajczak <git@mkljczk.pl>
2024-10-23 18:27:27 +0200

    Remove unused files
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- packages/pl-fe/src/normalizers/index.ts
+++
@@ -1,9 +0,0 @@
-export { normalizeAccount, type Account } from './account';
-export { normalizeAdminReport, type AdminReport } from './admin-report';
-export { normalizeChatMessage, type ChatMessage } from './chat-message';
-export { normalizeGroup, type Group } from './group';
-export { normalizeGroupMember, type GroupMember } from './group-member';
-export { normalizeNotification, type Notification } from './notification';
-export { normalizeStatus, type Status } from './status';
-
-export { PlFeConfigRecord, normalizePlFeConfig } from './pl-fe/pl-fe-config';
